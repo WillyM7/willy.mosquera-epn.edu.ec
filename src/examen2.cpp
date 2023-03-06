6045de4b6c10f10c138171e789c6efa42e4998b6
WillyM7 <willymosqueradg7@gmail.com>
2023-03-06 14:20:58 -0500

    part ok

--- src/examen2.cpp
+++ src/examen2.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
+
+const int WMGLOBAL_CONST = 1754217899;
+const string WMGLOBAL_STRING_CONST = "Willy Damian Mosquera Galeano";
+
+struct Coordenada {
+    int WMcapacidad_bélica;
+    string WMgeolocalización;
+    string WMdetalle_arsenal;
+    Coordenada *izquierda;
+    Coordenada *derecha;
+};
