4db040384fd1edcf1dfd0be45eb9202fbf5e4ab7
WillyM7 <willymosqueradg7@gmail.com>
2023-03-06 15:17:58 -0500

    adelanto

--- src/examen2.cpp
+++ src/examen2.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
-#include <cstdlib>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
@@ -15,3 +17,90 @@ struct Coordenada {
     Coordenada *izquierda;
     Coordenada *derecha;
 };
+
+struct Coordenada {
+    int cap;
+    string geo;
+    string tipo_arsenal;
+    Coordenada* izq;
+    Coordenada* der;
+};
+
+void WMagregarCoordenada(Coordenada* &raiz, int cap, string geo, string tipo_arsenal) {
+    if (raiz == NULL) {
+        raiz = new Coordenada;
+        raiz->cap = cap;
+        raiz->geo = geo;
+        raiz->tipo_arsenal = tipo_arsenal;
+        raiz->izq = NULL;
+        raiz->der = NULL;
+    }
+    else if (cap < raiz->cap) {
+        WMagregarCoordenada(raiz->izq, cap, geo, tipo_arsenal);
+    }
+    else if (cap > raiz->cap) {
+        WMagregarCoordenada(raiz->der, cap, geo, tipo_arsenal);
+    }
+    // Si la capacidad es igual, se descarta la coordenada repetida
+}
+
+void WMcargarCoordenadas(string nombre_archivo, Coordenada* &raiz) {
+    ifstream archivo(nombre_archivo);
+    if (!archivo.is_open()) {
+        cerr << "Error al abrir el archivo" << endl;
+        return;
+    }
+    string linea;
+    int contador = 0;
+    while (getline(archivo, linea)) {
+        // Mostrar loading de carga
+        cout << "Loading: " << contador << "%" << endl;
+        for (int i = 0; i <= contador; i++) {
+            cout << "=";
+        }
+        for (int i = contador + 1; i <= 100; i++) {
+            cout << " ";
+        }
+        cout << "|" << endl;
+        contador += 10;
+        // Leer la línea y validar si es una coordenada
+        istringstream ss(linea);
+        int cap;
+        string geo;
+        string tipo_arsenal;
+        if (ss >> cap >> geo >> tipo_arsenal) {
+            // Si es una coordenada, agregarla al árbol binario
+            WMagregarCoordenada(raiz, cap, geo, tipo_arsenal);
+            cout << "\033[32m" << linea << "\033[0m" << endl; // Mostrar en color verde
+        }
+        else {
+            // Si no es una coordenada, mostrar error en color rojo
+            cout << "\033[31m" << "Error en la línea: " << linea << "\033[0m" << endl;
+        }
+        this_thread::sleep_for(chrono::milliseconds(500)); // Simular una pausa en la carga
+    }
+    archivo.close();
+}
+
+int main() {
+    Coordenada* raiz = NULL;
+    WMcargarCoordenadas("coordenadas.txt", raiz);
+    return 0;
+}
+
+void WMInformacion(){
+    cout<<"[+] Informacion Arbol Binario de capacidad belica Ucrania"<<endl;
+    cout<<"Desarrollador-Nombre : "<<WMGLOBAL_CONST<<endl;;
+    cout<<"Desarrollador-Cedula : "<<WMGLOBAL_STRING_CONST<<endl;;
+    cout<<"Capacidad Belica : 36"<<endl;
+    cout<<"Coordenada Total : 7"<<endl;
+    cout<<"Coordenada-SecCarga : 1 7 5 4 2 8 9"<<endl;
+}
+
+int main()
+
+{
+    WMagregarCoordenada();
+    WMcargarCoordenadas();
+    WMInformacion();
+}
